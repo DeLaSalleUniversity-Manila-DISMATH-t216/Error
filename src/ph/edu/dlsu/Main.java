c3fb88577faac8ae20a9e1c2698612a7cd88f23f
JC Bautista <JC Bautista>
2016-02-16 21:33:15 -0800

    Initial commit

---
+++ src/ph/edu/dlsu/Main.java
@@ -0,0 +1,14 @@
+package ph.edu.dlsu;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String s = null;
+        System.out.println(s);
+
+        String[] strings = {"Welcome!"};
+        System.out.println(strings[1]);
+
+
+    }
+}
